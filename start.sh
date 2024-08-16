2efc700dbc82440dc2837cc63642cf047ce5f24e
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2024-08-16 13:34:00 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sudharsan104/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/Priyanka9760/Aj_Flix_Hub_Fliter_Bot.git /Aj_Flix_Hub_Fliter_Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TheMovieProviderBot
